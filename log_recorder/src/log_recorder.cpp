941cdae29f702bf65eab695918f6f175b0f94289
curtisburke-eng <curtisburke.eng@gmail.com>
2024-10-03 12:29:59 -0600

    Create service triggers

--- log_recorder/src/log_recorder.cpp
+++ log_recorder/src/log_recorder.cpp
@@ -1,40 +1,105 @@
 #include <rclcpp/rclcpp.hpp>
-#include <std_msgs/msg/string.hpp>
-
 #include <rosbag2_cpp/writer.hpp>
+#include <std_msgs/msg/string.hpp>
+#include <tf2_msgs/msg/TFMessage.hpp>
+#include <std_srvs/srv/trigger.hpp>
+#include <chrono>
+#include <sstream>
+#include <iomanip>
+#include <filesystem>
 
-using std::placeholders::_1;
-
-class LogManager : public rclcpp::Node
-{
+class LogManager : public rclcpp::Node {
 public:
-  LogManager()
-  : Node("log_manager")
-  {
-    writer_ = std::make_unique<rosbag2_cpp::Writer>();
+    LogManager()
+        : Node("log_manager"), recording_(false) {
+        
+        // Service for starting recording
+        start_service_ = this->create_service<std_srvs::srv::Trigger>(
+            "start_log_recording",
+            std::bind(&LogManager::startRecording, this, std::placeholders::_1, std::placeholders::_2)
+        );
 
-    writer_->open("my_bag");
+        // Service for stopping recording
+        stop_service_ = this->create_service<std_srvs::srv::Trigger>(
+            "stop_log_recording",
+            std::bind(&LogManager::stopRecording, this, std::placeholders::_1, std::placeholders::_2)
+        );
 
-    subscription_ = create_subscription<std_msgs::msg::String>(
-      "chatter", 10, std::bind(&LogManager::topic_callback, this, _1));
-  }
+        // Subscribe to multiple topics
+        auto tf_sub = this->create_subscription<tf2_msgs::msg::TFMessage>(
+            "tf", 10,
+            [this](const tf2_msgs::msg::TFMessage::SharedPtr msg) {
+                if (recording_) {
+                    recorder_.write(*msg);
+                    RCLCPP_INFO(this->get_logger(), "Recorded tf message");
+                }
+            });
+
+        auto tf_static_sub = this->create_subscription<tf2_msgs::msg::TFMessage>(
+            "tf_static", 10,
+            [this](const tf2_msgs::msg::TFMessage::SharedPtr msg) {
+                if (recording_) {
+                    recorder_.write(*msg);
+                    RCLCPP_INFO(this->get_logger(), "Recorded tf_static message");
+                }
+            });
+
+        RCLCPP_INFO(this->get_logger(), "Log Manager Node Initialized");
+    }
 
 private:
-  void topic_callback(std::shared_ptr<rclcpp::SerializedMessage> msg) const
-  {
-    rclcpp::Time time_stamp = this->now();
+    void startRecording(const std::shared_ptr<std_srvs::srv::Trigger::Request> request,
+                        std::shared_ptr<std_srvs::srv::Trigger::Response> response) {
+        if (recording_) {
+            response->success = false;
+            response->message = "Recording is already in progress.";
+            return;
+        }
 
-    writer_->write(msg, "chatter", "std_msgs/msg/String", time_stamp);
-  }
+        // Generate unique filename
+        std::string filename = generateUniqueFilename();
+        recorder_ = std::make_shared<rosbag2_cpp::LogManager>(filename);
+        recorder_->start();
 
-  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
-  std::unique_ptr<rosbag2_cpp::Writer> writer_;
+        recording_ = true;
+        response->success = true;
+        response->message = "Started recording to " + filename;
+        RCLCPP_INFO(this->get_logger(), "Started recording to %s", filename.c_str());
+    }
+
+    void stopRecording(const std::shared_ptr<std_srvs::srv::Trigger::Request> request,
+                       std::shared_ptr<std_srvs::srv::Trigger::Response> response) {
+        if (!recording_) {
+            response->success = false;
+            response->message = "No recording is in progress.";
+            return;
+        }
+
+        recorder_->stop();
+        recording_ = false;
+
+        response->success = true;
+        response->message = "Stopped recording.";
+        RCLCPP_INFO(this->get_logger(), "Stopped recording.");
+    }
+
+    std::string generateUniqueFilename() {
+        auto now = std::chrono::system_clock::now();
+        auto time = std::chrono::system_clock::to_time_t(now);
+        std::stringstream ss;
+        ss << "log_recording_" << std::put_time(std::localtime(&time), "%Y%m%d_%H%M%S") << ".db3";
+        return ss.str();
+    }
+
+    bool recording_;
+    std::shared_ptr<rosbag2_cpp::LogManager> recorder_;
+    rclcpp::Service<std_srvs::srv::Trigger>::SharedPtr start_service_;
+    rclcpp::Service<std_srvs::srv::Trigger>::SharedPtr stop_service_;
 };
 
-int main(int argc, char * argv[])
-{
-  rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<LogManager>());
-  rclcpp::shutdown();
-  return 0;
+int main(int argc, char * argv[]) {
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<LogManager>());
+    rclcpp::shutdown();
+    return 0;
 }
