f7ab6372a751f70dbbac1c88c56e27e6ac0ae23f
curtisburke-eng <curtisburke.eng@gmail.com>
2024-10-03 12:31:22 -0600

    Create subscription callbacks

--- log_recorder/src/log_recorder.cpp
+++ log_recorder/src/log_recorder.cpp
@@ -1,8 +1,11 @@
 #include <rclcpp/rclcpp.hpp>
 #include <rosbag2_cpp/writer.hpp>
-#include <std_msgs/msg/string.hpp>
-#include <tf2_msgs/msg/TFMessage.hpp>
 #include <std_srvs/srv/trigger.hpp>
+
+#include <geometry_msgs/msg/transform_stamped.hpp>
+#include <tf2_ros/buffer.h>
+#include <tf2_ros/transform_listener.h>
+
 #include <chrono>
 #include <sstream>
 #include <iomanip>
@@ -11,46 +14,36 @@
 class LogManager : public rclcpp::Node {
 public:
     LogManager()
-        : Node("log_manager"), recording_(false) {
+        : Node("log_manager"), isRecording(false) {
         
         // Service for starting recording
-        start_service_ = this->create_service<std_srvs::srv::Trigger>(
+        startService = this->create_service<std_srvs::srv::Trigger>(
             "start_log_recording",
             std::bind(&LogManager::startRecording, this, std::placeholders::_1, std::placeholders::_2)
         );
 
         // Service for stopping recording
-        stop_service_ = this->create_service<std_srvs::srv::Trigger>(
+        stopService = this->create_service<std_srvs::srv::Trigger>(
             "stop_log_recording",
             std::bind(&LogManager::stopRecording, this, std::placeholders::_1, std::placeholders::_2)
         );
 
         // Subscribe to multiple topics
-        auto tf_sub = this->create_subscription<tf2_msgs::msg::TFMessage>(
-            "tf", 10,
-            [this](const tf2_msgs::msg::TFMessage::SharedPtr msg) {
-                if (recording_) {
-                    recorder_.write(*msg);
-                    RCLCPP_INFO(this->get_logger(), "Recorded tf message");
-                }
-            });
+        tfSub = this->create_subscription<tf2_msgs::msg::TFMessage>(
+            "tf", 10,std::bind(&LogManager::tfSubCallback, this, std::placeholders::_1));
 
-        auto tf_static_sub = this->create_subscription<tf2_msgs::msg::TFMessage>(
-            "tf_static", 10,
-            [this](const tf2_msgs::msg::TFMessage::SharedPtr msg) {
-                if (recording_) {
-                    recorder_.write(*msg);
-                    RCLCPP_INFO(this->get_logger(), "Recorded tf_static message");
-                }
-            });
+        tfStaticSub = this->create_subscription<tf2_msgs::msg::TFMessage>(
+            "tf_static", 10,std::bind(&LogManager::tfStaticSubCallback, this, std::placeholders::_1));
+
 
         RCLCPP_INFO(this->get_logger(), "Log Manager Node Initialized");
     }
 
 private:
+    // Service Trigger Functions
     void startRecording(const std::shared_ptr<std_srvs::srv::Trigger::Request> request,
                         std::shared_ptr<std_srvs::srv::Trigger::Response> response) {
-        if (recording_) {
+        if (isRecording) {
             response->success = false;
             response->message = "Recording is already in progress.";
             return;
@@ -58,10 +51,10 @@ private:
 
         // Generate unique filename
         std::string filename = generateUniqueFilename();
-        recorder_ = std::make_shared<rosbag2_cpp::LogManager>(filename);
-        recorder_->start();
+        // Start recording to filename
+        recorder->open(filename);
 
-        recording_ = true;
+        isRecording = true;
         response->success = true;
         response->message = "Started recording to " + filename;
         RCLCPP_INFO(this->get_logger(), "Started recording to %s", filename.c_str());
@@ -69,14 +62,14 @@ private:
 
     void stopRecording(const std::shared_ptr<std_srvs::srv::Trigger::Request> request,
                        std::shared_ptr<std_srvs::srv::Trigger::Response> response) {
-        if (!recording_) {
+        if (!isRecording) {
             response->success = false;
             response->message = "No recording is in progress.";
             return;
         }
-
-        recorder_->stop();
-        recording_ = false;
+        // Stop recording
+        recorder->close();
+        isRecording = false;
 
         response->success = true;
         response->message = "Stopped recording.";
@@ -85,16 +78,33 @@ private:
 
     std::string generateUniqueFilename() {
         auto now = std::chrono::system_clock::now();
-        auto time = std::chrono::system_clock::to_time_t(now);
+        auto time = std::chrono::system_clock::to_time_t(now);    
         std::stringstream ss;
+
         ss << "log_recording_" << std::put_time(std::localtime(&time), "%Y%m%d_%H%M%S") << ".db3";
         return ss.str();
     }
 
-    bool recording_;
-    std::shared_ptr<rosbag2_cpp::LogManager> recorder_;
-    rclcpp::Service<std_srvs::srv::Trigger>::SharedPtr start_service_;
-    rclcpp::Service<std_srvs::srv::Trigger>::SharedPtr stop_service_;
+    // Subscription Callback functions
+    void tfSubCallback(std::shared_ptr<rclcpp::SerializedMessage> msg) const {
+        rclcpp::Time timestamp = this->now();
+
+        recorder->write(msg, "tf", "tf2_msgs/msg/TFMessage", timestamp);
+    }
+    void tfStaticSubCallback(std::shared_ptr<rclcpp::SerializedMessage> msg) const {
+        rclcpp::Time timestamp = this->now();
+
+        recorder->write(msg, "tf_static", "tf2_msgs/msg/TFMessage", timestamp);
+    }
+
+    bool isRecording;
+    std::shared_ptr<rosbag2_cpp::Writer> recorder;
+    rclcpp::Service<std_srvs::srv::Trigger>::SharedPtr startService;
+    rclcpp::Service<std_srvs::srv::Trigger>::SharedPtr stopService;
+    // Declare Subscriptions
+    rclcpp::Subscription<tf2_msgs::msg::TFMessage>::SharedPtr tfSub;
+    rclcpp::Subscription<tf2_msgs::msg::TFMessage>::SharedPtr tfStaticSub;
+    
 };
 
 int main(int argc, char * argv[]) {
